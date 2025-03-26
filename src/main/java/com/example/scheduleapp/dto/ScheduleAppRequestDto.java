3e801eeca844f470a6ba37a0b516de14693c3d4b
kkio22 <nahyeon2252@gmail.com>
2025-03-26 15:29:45 +0900

    Update ScheduleAppRequestDto.java

--- src/main/java/com/example/scheduleapp/dto/ScheduleAppRequestDto.java
+++ src/main/java/com/example/scheduleapp/dto/ScheduleAppRequestDto.java
@@ -12,8 +12,8 @@ import java.time.LocalDateTime;
 public class ScheduleAppRequestDto {
     private String task;
     private String author_name;
-    private String password; //비밀번호는 String으로 처리함(보안상의 문제 때문임)
-    private LocalDateTime created_date;  //날짜나 시간은 LocalDateTime으로 자료형을 적어야 함
+    private String password; 
+    private LocalDateTime created_date;  
     private LocalDateTime update_date;
 
 
