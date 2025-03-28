19f631a49f3a8a1d2a2ab98ea26c00c2e0ff48cf
kkio22 <nahyeon2252@gmail.com>
2025-03-29 03:40:41 +0900

    Update and rename JdbcTemplateScheduleAppRepository.java to JdbcTemplateScheduleRepository.java

---
+++ src/main/java/com/example/scheduleapp/repository/JdbcTemplateScheduleRepository.java
@@ -0,0 +1,106 @@
+package com.example.scheduleapp.repository;
+
+
+import com.example.scheduleapp.dto.ScheduleResponseDto;
+import com.example.scheduleapp.entity.Schedule;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+
+public class JdbcTemplateScheduleRepository implements ScheduleRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public JdbcTemplateScheduleRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+    @Override
+    public ScheduleResponseDto saveRepository(Schedule schedule) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("scheduleApp").usingGeneratedKeyColumns("id");
+        LocalDateTime now = LocalDateTime.now();//static
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("todo", schedule.getTodo());
+        parameters.put("name", schedule.getName());
+        parameters.put("password", schedule.getPassword());
+        parameters.put("created", now);
+        parameters.put("updated", now);
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+
+        return new ScheduleResponseDto(key.longValue(), schedule.getTodo(), schedule.getName(), schedule.getPassword(), now, now);
+
+
+    }
+
+    @Override
+    public List<ScheduleResponseDto> checkRepository() {//entity 들어왔어 service에서
+        return jdbcTemplate.query("select*from schedule where = 'updated' desc ", scheduleRowMapper()); //jdbcTemplate.query jdbcTemplate 매서드/sql문 넣고,
+    }
+
+
+    private RowMapper<ScheduleResponseDto> scheduleRowMapper(){ //ScheduleResponse 객체에 데이터베이스 객체를 매핑해서 RowMapper를 이용해 반환하겠다. //물어봐야겠다.
+        return new RowMapper<ScheduleResponseDto>(){
+
+            @Override
+            public ScheduleResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new ScheduleResponseDto(
+                        rs.getLong("id"),
+                        rs.getString("todo"),
+                        rs.getString("name"),
+                        rs.getString("password"),
+                        rs.getTimestamp("create").toLocalDateTime(),
+                        rs.getTimestamp("update").toLocalDateTime()
+                );
+
+            }
+        };
+
+    }
+
+
+
+
+
+    @Override
+    public Schedule oneCheckRepository(long id) {
+        List<Schedule> result=jdbcTemplate.query("select*from schedule where id= ?", ScheduleRowMapperV2(),id);
+//jdbcTemplate.query는 List로 값을 반환한다.
+        return result.stream().findAny().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist id = " + id));
+    }
+
+    private RowMapper<Schedule> ScheduleRowMapperV2() {
+        return new RowMapper<Schedule>() {
+
+            @Override
+            public Schedule mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Schedule(
+                        rs.getLong("id"),
+                        rs.getString("todo"),
+                        rs.getString("name"),
+                        rs.getString("password"),
+                        rs.getTimestamp("create").toLocalDateTime(),
+                        rs.getTimestamp("update").toLocalDateTime()
+                );
+            }
+        };
+    };
+
+}
+
