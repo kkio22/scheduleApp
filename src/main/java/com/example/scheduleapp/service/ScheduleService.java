01e7d425fd0f7446a026811f14e1200b3427f53c
kkio22 <nahyeon2252@gmail.com>
2025-03-29 03:41:34 +0900

    Update and rename ScheduleAppService.java to ScheduleService.java

---
+++ src/main/java/com/example/scheduleapp/service/ScheduleService.java
@@ -0,0 +1,16 @@
+package com.example.scheduleapp.service;
+
+
+import com.example.scheduleapp.dto.ScheduleRequestDto;
+import com.example.scheduleapp.dto.ScheduleResponseDto;
+
+import java.util.List;
+
+public interface ScheduleService {
+    ScheduleResponseDto saveService(ScheduleRequestDto dto);
+
+    List<ScheduleResponseDto> checkService ();
+
+    ScheduleResponseDto oneCheckService (long id);
+
+}
