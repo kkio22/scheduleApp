b4373e12a03070a0b8cc4d72472f29f9c3c8e891
kkio22 <nahyeon2252@gmail.com>
2025-03-29 03:38:25 +0900

    Update and rename ScheduleAppController.java to SchedulController.java

---
+++ src/main/java/com/example/scheduleapp/controller/SchedulController.java
@@ -0,0 +1,46 @@
+package com.example.scheduleapp.controller;
+
+
+import com.example.scheduleapp.dto.ScheduleRequestDto;
+import com.example.scheduleapp.dto.ScheduleResponseDto;
+import com.example.scheduleapp.service.ScheduleService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/schedules")
+public class ScheduleController {
+    private final ScheduleService scheduleService; //응답에 필요
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ScheduleResponseDto> createSchedule (
+            ScheduleRequestDto dto
+    ) {
+        return new ResponseEntity<>(scheduleService.saveService(dto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<ScheduleResponseDto> checkSchedule (
+            @RequestParam(required=false)String name,
+            @RequestParam(required=false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime update
+    ){ //@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)이거 찾아보기
+        return scheduleService.checkService(); //왜 얘는 new 안 해요?
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ScheduleResponseDto> oneCheckSchedule (
+            @PathVariable long id
+    ){
+        return new ResponseEntity<ScheduleResponseDto>(scheduleService.oneCheckService(id), HttpStatus.OK);
+    }
+
+}
