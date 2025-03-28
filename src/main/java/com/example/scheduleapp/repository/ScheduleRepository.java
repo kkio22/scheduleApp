3c222941363b4f3257c8579320b28a71533718e3
kkio22 <nahyeon2252@gmail.com>
2025-03-29 03:41:04 +0900

    Update and rename ScheduleAppRepository.java to ScheduleRepository.java

---
+++ src/main/java/com/example/scheduleapp/repository/ScheduleRepository.java
@@ -0,0 +1,14 @@
+package com.example.scheduleapp.repository;
+
+import com.example.scheduleapp.dto.ScheduleResponseDto;
+import com.example.scheduleapp.entity.Schedule;
+
+import java.util.List;
+
+public interface ScheduleRepository {
+    ScheduleResponseDto saveRepository(Schedule schedule);
+
+    List<ScheduleResponseDto> checkRepository();
+
+    Schedule oneCheckRepository(long id);
+}
