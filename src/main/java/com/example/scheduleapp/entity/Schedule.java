24f798a9e773b683e3cc6fb0b40d3387594183e9
kkio22 <nahyeon2252@gmail.com>
2025-03-29 03:40:09 +0900

    Update and rename ScheduleApp.java to Schedule.java

---
+++ src/main/java/com/example/scheduleapp/entity/Schedule.java
@@ -0,0 +1,21 @@
+package com.example.scheduleapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+    @Getter
+    @AllArgsConstructor
+
+    public class Schedule {
+        private long id;
+        private String todo;
+        private String name;
+        private String password;
+        private LocalDateTime create;
+        private LocalDateTime update;
+
+    }
+
+
